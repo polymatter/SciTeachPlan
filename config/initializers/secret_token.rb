14008376e0f17b36891ec3e17dda532ead73a435
Jeffrey D.S. Lake <glisher_rock@hotmail.com>
2011-11-13 20:11:24 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sciteachplan::Application.config.secret_token = 'bb1741c304a7bcb5e1ad5ea2ad291e1e4ef1c2b07fbd34c1c68de2a78be3a2a00c59d9e1d1797613a1dac2b14449d170f1410c22b1d86e65d4ec09c5d328ef63'
